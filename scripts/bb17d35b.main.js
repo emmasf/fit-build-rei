6faa3a683d75dab98cb6764420962437a682b1be
Ben Kutil <ben@make-things.com>
2014-02-12 15:20:59 -0500

    build from 46a0ce9

---
+++ scripts/bb17d35b.main.js
@@ -0,0 +1,1 @@
+$(".jcarousel").jcarousel({wrap:"circular",list:"> ul"}).jcarousel({vertical:!0,list:"> ul > li > ul"}),$(".jcarousel-vertical-control-prev").jcarouselControl({target:"-=1"}),$(".jcarousel-vertical-control-next").jcarouselControl({target:"+=1"});
